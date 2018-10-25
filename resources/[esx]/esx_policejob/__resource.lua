b3e9c0922e53ee14dc20afe89b5eba6072e01837
raitrax <maxime.maurer@viacesi.fr>
2018-10-25 21:18:48 +0200

    police update

--- resources/[esx]/esx_policejob/__resource.lua
+++ resources/[esx]/esx_policejob/__resource.lua
@@ -2,29 +2,38 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Police Job'
 
-version '1.0.4'
+version '1.2.0'
 
 server_scripts {
-  '@es_extended/locale.lua',
-  'locales/br.lua',
-  'locales/en.lua',
-  'locales/fr.lua',
-  'locales/es.lua',
-  '@mysql-async/lib/MySQL.lua',
-  'config.lua',
-  'server/main.lua'
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/pl.lua',
+	'locales/fr.lua',
+	'locales/fi.lua',
+	'locales/es.lua',
+	'locales/sv.lua',
+	'config.lua',
+	'server/main.lua'
 }
 
 client_scripts {
-  '@es_extended/locale.lua',
-  'locales/br.lua',
-  'locales/en.lua',
-  'locales/fr.lua',
-  'locales/es.lua',
-  'config.lua',
-  'client/main.lua'
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/pl.lua',
+	'locales/fr.lua',
+	'locales/fi.lua',
+	'locales/es.lua',
+	'locales/sv.lua',
+	'config.lua',
+	'client/main.lua'
 }
 
-exports {
-  'getJob' 
+dependencies {
+	'es_extended',
+	'esx_billing'
 }
