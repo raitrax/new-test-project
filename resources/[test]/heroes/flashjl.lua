c56d3bdcb6a431ee0448f52ca5b3e250f39a2402
raitrax <maxime.maurer@viacesi.fr>
2018-11-04 17:47:56 +0100

    Update resources/[test]/heroes/flashjl.lua

--- resources/[test]/heroes/flashjl.lua
+++ resources/[test]/heroes/flashjl.lua
@@ -177,7 +177,7 @@ powercalled = true
 -- PrintChatMessage("Step 1")
 
         Wait(1)
-		skin = "FlashJL"
+		skin = "flashn52"
 		local model = GetHashKey(skin)
     RequestModel(model)
     while not HasModelLoaded(model) do -- Wait for model to load
